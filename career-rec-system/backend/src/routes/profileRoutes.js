12bfeb07878cd9477424b1eebedd8f502230953a
Mowneeshs29 <mowneesh.cs23@bitsathy.ac.in>
2026-02-11 09:51:59 +0530

    Delete career-rec-system/backend/src directory

--- career-rec-system/backend/src/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getProfile, updateProfile } = require("../controllers/profileController");
-const { verifyToken } = require("../middleware/authMiddleware");
-
-router.get("/", verifyToken, getProfile);
-router.put("/", verifyToken, updateProfile);
-
-module.exports = router;
